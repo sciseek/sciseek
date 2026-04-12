ccee4106e3ddbcc8bd3e0b6c97064f581e667426
SciSeek <admin@sciseek.com>
2026-04-11 19:23:52 -0500

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -16,6 +16,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "https://sciseek-app.vercel.app",
     ],
     allow_origin_regex=r"https://.*\.vercel\.app",
     allow_credentials=True,
