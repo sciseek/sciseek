1e8d8a5b8c89fd1514d905a2242a7d6ad89dc397
SciSeek <admin@sciseek.com>
2026-04-12 15:07:19 -0500

    Update db.py

--- backend/app/db.py
+++ backend/app/db.py
@@ -1,13 +1,14 @@
 import os
 from dotenv import load_dotenv
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-print("DATABASE_URL =", DATABASE_URL)
-
-from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL is not set.")
 
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
