2c2845495ffea4f93edae8b7ede55accbbd51f14
SciSeek <admin@sciseek.com>
2026-04-13 17:41:18 -0500

    Update layout.tsx

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
-	  <GoogleAnalytics gaId="G-396713125" />
+	  <GoogleAnalytics gaId="G-S8HY1NX2BR" />
     </html>
   );
 }
