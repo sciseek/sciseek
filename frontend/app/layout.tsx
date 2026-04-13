d5ef31e65dc5d3e40dc4de9c3b6177e72e45448b
SciSeek <admin@sciseek.com>
2026-04-13 17:11:12 -0500

    Update layout.tsx

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,16 +1,11 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
 import { GoogleAnalytics } from '@next/third-parties/google'
- 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+
+export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html>
       <body>{children}</body>
       <GoogleAnalytics gaId="G-396713125" />
     </html>
